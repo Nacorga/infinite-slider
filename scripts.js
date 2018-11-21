53a94891330a61e88c40c10f0b5d8c424e248f4c
ignacio-cortes-externo <ignacio.cortes.externo@axa.com>
2018-11-21 08:20:23 +0100

    Complete implementation

--- scripts.js
+++ scripts.js
@@ -1,86 +1,68 @@
-let xs = 480;
-let sm = 768;
-let md = 992;
-let lg = 1200;
-
 $( document ).ready(function() {
 
   let bxsliderElems = $('.elements');
   let listBox = $('.bxslider');
   let bxsliderElemsLength = bxsliderElems.length;
 
-  //MOBILE
-  if (window.innerWidth <= xs) {
-
-    bxsliderElems.css('width', 28.5 + '%');
-    listBox.css('width', (28.5*bxsliderElemsLength) + '%');
-
-  }
-
-  //TABLET
-  if ((window.innerWidth > xs && window.innerWidth < md) && bxsliderElemsLength > 8) {
-
-    bxsliderElems.css('width', 28.5 + '%');
-    listBox.css('width', (28.5*bxsliderElemsLength) + '%');
-    listBox.css('flex-wrap', 'nowrap');
-
-  }
-
-  //DESKTOP
-
-  let elemsPerSlide = 7;
+  let elemsPerSlide = 4; //DEFAULT VALUE
 
   $( "#num-slides" ).change(function(){
     elemsPerSlide = $( "#num-slides" ).val();
     blueSlider();
   });
 
-  if (bxsliderElemsLength > elemsPerSlide && window.innerWidth >= md) {
+  $('.slider-controls').css('display', 'flex');
 
-    $('.slider-controls').css('display', 'flex');
+  let prevArrow = $('.slider-controls .slide-prev');
+  let nextArrow = $('.slider-controls .slide-next');
 
-    let prevArrow = $('.slider-controls .slide-prev');
-    let nextArrow = $('.slider-controls .slide-next');
+  let elemWidth;
+  let maxMargin;
+  let firstSeven;
+  let lastSeven;
+  let marginLeft;
+  let count;
+  let numSlides;
 
-    let elemWidth;
-    let maxMargin;
-    let firstSeven;
-    let lastSeven;
-    let marginLeft;
-    let count;
-    let numSlides;
+  let isComplete = true;
 
-    blueSlider();
+  blueSlider();
 
-    function blueSlider() {
+  function blueSlider() {
 
-      $( 'li.elements' ).remove('.cloneBefore').removeClass('cloneBefore');
-      $( 'li.elements' ).remove('.cloneAfter').removeClass('cloneAfter');
-      
-      elemWidth = 100 / elemsPerSlide;
-      maxMargin = 100;
-      marginLeft = -maxMargin;
+    $( 'li.elements' ).remove('.cloneBefore').removeClass('cloneBefore');
+    $( 'li.elements' ).remove('.cloneAfter').removeClass('cloneAfter');
+    
+    elemWidth = 100 / elemsPerSlide;
+    maxMargin = 100;
+    marginLeft = -maxMargin;
 
-      bxsliderElems = $('.elements');
-      bxsliderElemsLength = bxsliderElems.length;
+    bxsliderElems = $('.elements');
+    bxsliderElemsLength = bxsliderElems.length;
 
-      cloneBefore = bxsliderElems.slice(bxsliderElemsLength - elemsPerSlide,bxsliderElemsLength);
-      cloneAfter = bxsliderElems.slice(0,elemsPerSlide);
-      cloneBefore.clone().addClass('cloneBefore').prependTo(listBox);
-      cloneAfter.clone().addClass('cloneAfter').appendTo(listBox);
+    cloneBefore = bxsliderElems.slice(bxsliderElemsLength - elemsPerSlide,bxsliderElemsLength);
+    cloneAfter = bxsliderElems.slice(0,elemsPerSlide);
+    cloneBefore.clone().addClass('cloneBefore').prependTo(listBox);
+    cloneAfter.clone().addClass('cloneAfter').appendTo(listBox);
 
-      bxsliderElems = $('.elements');
-      bxsliderElemsLength = bxsliderElems.length;
-      bxsliderElems.css('width', elemWidth + '%');
-      listBox.css('width', (elemWidth*bxsliderElemsLength) + '%');
-      listBox.css('marginLeft', -maxMargin + '%');
+    bxsliderElems = $('.elements');
+    bxsliderElemsLength = bxsliderElems.length;
+    bxsliderElems.css('width', elemWidth + '%');
+    listBox.css('width', (elemWidth*bxsliderElemsLength) + '%');
+    listBox.css('marginLeft', -maxMargin + '%');
 
-      count = 1;
-      numSlides = Math.floor( (bxsliderElemsLength - elemsPerSlide*2) / elemsPerSlide );
+    count = 1;
+    numSlides = Math.floor( (bxsliderElemsLength - elemsPerSlide*2) / elemsPerSlide );
 
-    }
+  }
 
-    nextArrow.click(function(){
+  nextArrow.click(function(){
+
+    if ( isComplete ) {
+
+      console.log('preue');
+
+      isComplete = false;
 
       if (bxsliderElemsLength % elemsPerSlide != 0) {
 
@@ -126,11 +108,19 @@ $( document ).ready(function() {
 
       listBox.animate({
         marginLeft: marginLeft + '%'
-      }, 1000);
+      }, 1000, function() {
+        isComplete = true;
+      });
 
-    });
+    }
 
-    prevArrow.click(function(){
+  });
+
+  prevArrow.click(function(){
+
+    if ( isComplete ) {
+
+      isComplete = false;
 
       if (bxsliderElemsLength % elemsPerSlide != 0) {
 
@@ -167,10 +157,12 @@ $( document ).ready(function() {
 
       listBox.animate({
         marginLeft: marginLeft + '%'
-      }, 1000);
+      }, 1000, function() {
+        isComplete = true;
+      });
 
-    });
+    }
 
-  }
+  });
 
 });
